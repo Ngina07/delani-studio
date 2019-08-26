cfa6966071a110ad309291d9afd5a495b9fca9b4
Ngina07 <phillis.njoroge@strathmore.edu>
2019-08-26 11:56:34 +0300

    fix bug that was causing forms not to validate

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,53 @@
 $(document).ready(function () {
-    //Portfolio label on hover effect
-    $('#portfolio span').addClass("hide");
-    $('#portfolio .doings').hover(function () {
-        /*fade in code*/
-        $(this).find('span').removeClass('hide');
-        $(this).find('.doings').addClass('imageEffect');
-    }, function () {
-        /*fade out code*/
-        $(this).find('span').addClass('hide');
-        $(this).find('.doings').removeClass('imageEffect');
-    });
-    // What we do toggle styling
-
-    $(".first").click(function () {
-        $("#design-icon").toggle(100);
-        $("#design").toggle(100);
-    });
-    $(".second").click(function () {
-        $("#dev-icon").toggle(100);
-        $("#developement").toggle(100);
-    });
-    $(".third").click(function () {
-        $("#product-icon").toggle(100);
-        $("#product-management").toggle(100);
-    });
-    //Submission Form
-  $('#submit').submit(function(event) {
-    var name = $('#name').val();
-    var email = $('#inputEmail').val();
-    var message = $('#message').val();
-    if (name == "" || email == "" || message == "") {
-      $("#message").text("Please fill in all the fields with correct data.");
-      $('#message').removeClass("success");
-      $('#message').addClass("failed");
-    } else {
-      $("#message").text("Thank you " + name + ".<br>Your message has been successfully submitted.");
-      $('#message').removeClass("failed");
-      $('#message').addClass("success");
-    }
-    event.preventDefault();
+  //Portfolio label on hover effect
+  $('#portfolio span').addClass("hide");
+  $('#portfolio .doings').hover(function () {
+    /*fade in code*/
+    $(this).find('span').removeClass('hide');
+    $(this).find('.doings').addClass('imageEffect');
+  }, function () {
+    /*fade out code*/
+    $(this).find('span').addClass('hide');
+    $(this).find('.doings').removeClass('imageEffect');
   });
+  // What we do toggle styling
+
+  $(".first").click(function () {
+    $("#design-icon").toggle(100);
+    $("#design").toggle(100);
+  });
+  $(".second").click(function () {
+    $("#dev-icon").toggle(100);
+    $("#developement").toggle(100);
+  });
+  $(".third").click(function () {
+    $("#product-icon").toggle(100);
+    $("#product-management").toggle(100);
+  });
+  //Submission Form
+  $(document).ready(function () {
+    $("#submit").click(function (event) {
+      console.log("working")
+      event.preventDefault();
+      var name = document.getElementById("name").value;
+      var email = document.getElementById("inputEmail").value;
+      var message = document.getElementById("message").value;
+      // form validation
+      if (name == "") {
+        alert("please enter your name");
+        return false;
+      } else if (email == "") {
+        alert("please enter your email")
+        return false;
+      } else if (message == "") {
+        alert("please tell us something")
+        return false;
+      } else {
+        alert(name + " your message is received. Thank you");
+      }
+      document.getElementById("contactform").reset();
+    });
+  });
+
 });
+
